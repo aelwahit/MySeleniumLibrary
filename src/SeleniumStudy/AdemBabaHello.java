502088786f3c4bfe8945b1f061b97fe70c1401f7
Adem Kahraman <aelwahit@gmail.com>
2019-07-01 22:47:15 -0500

    hello

---
+++ src/SeleniumStudy/AdemBabaHello.java
@@ -0,0 +1,12 @@
+package SeleniumStudy;
+
+public class AdemBabaHello {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=5 ; i++) {
+            System.out.println("hello "+i);
+        }
+
+
+    }
+}
