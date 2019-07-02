b96148a7ec7ef9fce0aaa507539e57965595e152
Adem Kahraman <aelwahit@gmail.com>
2019-07-01 22:21:54 -0500

    Initial commit

---
+++ src/SeleniumStudy/Library.java
@@ -0,0 +1,26 @@
+package SeleniumStudy;
+
+public class Library {
+    public static void main(String[] args) {
+
+
+    }
+
+ public static void sleep(double x
+ ){
+
+
+     try {
+         Thread.sleep(1000*(long) x);
+     } catch (InterruptedException e) {
+         e.printStackTrace();
+     }
+ }
+
+
+
+
+
+
+
+}
